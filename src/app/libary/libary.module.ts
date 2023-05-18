22f2b0db9d89344f9ee2314273d53c56e7c26c25
Edson Carlos <edson.carlos@dtidigital.com.br>
2023-05-18 18:31:47 -0300

    #03 feat: Criado Container, Content e Cards

--- src/app/libary/libary.module.ts
+++ src/app/libary/libary.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 import { LibaryRoutingModule } from './libary-routing.module';
 import { LibaryComponent } from './libary/libary.component';
 
 @NgModule({
   declarations: [LibaryComponent],
-  imports: [CommonModule, LibaryRoutingModule],
+  imports: [CommonModule, LibaryRoutingModule, MatCardModule],
 })
 export class LibaryModule {}
