da3a35d2aadbbb1592e33e8ba5f5dfd79c9efff3
Edson Carlos <edsoncarlos@ymail.com>
2023-05-29 02:12:22 -0300

    #05 refeact: Refatoração

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { SharedModule } from 'src/shared/shared.module';
-import { AppMaterialImportsModule } from '../shared/app-material-imports/app-material-imports.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,8 +14,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     HttpClientModule,
-    AppMaterialImportsModule,
     SharedModule,
   ],
   providers: [],
